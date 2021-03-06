3c12a1af74d92085ae404af15c6b8cf7e60fe24d
SteveDev <stevehoneck@gmail.com>
2021-03-06 00:28:53 -0500

    Post 'Exercise: Home Component'

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
+import { PROMOTIONS } from '../shared/promotions';
+import { PARTNERS } from '../shared/partners';
+
+function RenderItem({item}) { //passed props from which we destruct "item"
+    if (item) {
+        return (
+            <Card
+                featuredTitle={item.name}
+                image={require('./images/react-lake.jpg')}
+            >
+                <Text style={{margin: 10}}>
+                    {item.description}
+                </Text>
+            </Card>
+        );
+    }
+    return <View />;
+}
 
 class Home extends Component {
 
-    static navigationOptions = { //Sets up the screen title
+    constructor(props) {
+        super(props);
+        this.state = {
+            campsites: CAMPSITES,
+            promotions: PROMOTIONS,
+            partners: PARTNERS
+        };
+    }
+
+    static navigationOptions = { //Sets up screen title
         title: 'Home'
     }
 
     render() {
-        return (
-            <View>
-                <Text>Home Component</Text>
-            </View>
+        return ( //NOTE FOR LINE 41: Can be used to render groups of items like FlatList. ScrollView loads all at once, FlatList just renders the visible part
+            <ScrollView> 
+                <RenderItem 
+                    item={this.state.campsites.filter(campsite => campsite.featured)[0]} //Takes campsite data from state and searches for the ".featured" attribute that is set to true.
+                />
+                <RenderItem 
+                    item={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                />
+                <RenderItem 
+                    item={this.state.partners.filter(partner => partner.featured)[0]}
+                />
+            </ScrollView>
         );
     }
 }
