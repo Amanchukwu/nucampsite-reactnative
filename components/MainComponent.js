189812cee761f68b1c3bdfb7ae30f0cd3d6ee9d5
SteveDev <stevehoneck@gmail.com>
2021-03-10 22:05:31 -0500

    Post 'Exercise: Forms and Modals'

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,6 +4,7 @@ import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
+import Reservation from './ReservationComponent';
 import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
@@ -120,6 +121,29 @@ const ContactNavigator = createStackNavigator(
     }
 );
 
+const ReservationNavigator = createStackNavigator(
+    {
+        Reservation: { screen: Reservation }
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='tree'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
+
 const CustomDrawerContentComponent = props => ( //Recieve "props" as its parameter and will return a view of our customized drawer
     <ScrollView> 
         <SafeAreaView //specifically for the iPhoneX, takes into account the rounded corners and camera notch. Default drawer component already includes this. But has to be added here because we are overriding the default.
@@ -159,6 +183,20 @@ const MainNavigator = createDrawerNavigator( //Needs an object that contains the
                 drawerIcon: ({tintColor}) => (
                     <Icon
                         name='list'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+        Reservation: {
+            screen: ReservationNavigator,
+            navigationOptions: {
+                drawerLabel: 'Reserve Campsite',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='tree'
                         type='font-awesome'
                         size={24}
                         color={tintColor}
---
+++ components/ReservationComponent.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import { Text, View, ScrollView, StyleSheet,
+    Picker, Switch, Button } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+
+//Creating this form as a react controlled form where the form data is stored in and controlled by the component itself instead of redux
+class Reservation extends Component { 
+
+    constructor(props) {
+        super(props);
+
+        this.state = { //Initialze the values for each form input
+            campers: 1,
+            hikeIn: false,
+            date: new Date(), //JS built in "Date()" function
+            showCalendar: false
+        };
+    }
+
+    static navigationOptions = { //Will be a stack navigator screen, so add navigationOptions
+        title: 'Reserve Campsite'
+    }
+
+    handleReservation() { //Will need to handle when the form is submitted
+        console.log(JSON.stringify(this.state));//Just echo back the form data 
+        this.setState({ //Reset state (and therefore form) to its initial values
+            campers: 1,
+            hikeIn: false,
+            date: new Date(),
+            showCalendar: false     
+        });
+    }
+
+    render() {
+        return (
+            <ScrollView /*Create the form inside a <ScrollView> component */> 
+                <View style={styles.formRow} /*When building a form, put each form input inside of its own view*/>
+                    <Text style={styles.formLabel}>Number of Campers</Text>
+                    <Picker //Want to give the user the option to choose between 1 and 6 (would use Option and Select elements in HTML), use <Picker> in React Native
+                        style={styles.formItem}
+                        selectedValue={this.state.campers} //<Picker> comes with a built in prop called selectedValue which we set to
+                        onValueChange={itemValue => this.setState({campers: itemValue})}//<Picker> comes with a built in prop called "onValueChange" which we pass a callback funciton with a "itemValue" parameter and it will update the component's state's camper's property with that itemValue. When user selects a value, it will trigger this"onValueChange" prop to update the component's state with that item's value. Then the "selectedValue" prop will be updated to match the current state so the picker knows which item to display as the current selection.
+                    >
+                        <Picker.Item label='1' value='1' /* "label" is what the user sees, the "value" is what is passed to the "onValueChange" prop when the user makes a selection*//>
+                        <Picker.Item label='2' value='2' />
+                        <Picker.Item label='3' value='3' />
+                        <Picker.Item label='4' value='4' />
+                        <Picker.Item label='5' value='5' />
+                        <Picker.Item label='6' value='6' />
+                    </Picker>
+                </View>
+                <View style={styles.formRow}>
+                    <Text style={styles.formLabel}>Hike-In?</Text>
+                    <Switch //Built in React Native component <Switch> is designed
+                        style={styles.formItem} 
+                        value={this.state.hikeIn} //Built in prop, 
+                        trackColor={{true: '#5637DD', false: null}} //Built in prop, give a color if the value is true or false
+                        onValueChange={value => this.setState({hikeIn: value})} //Built in prop, triggered when the user changes the value. This prop takes a callback funciton to which we pass the "value" parameter which contains the value the user has changed to and it will update the ReservationComponents's state with a new value.
+                    />
+                </View>
+                <View style={styles.formRow}>
+                    <Text style={styles.formLabel}>Date</Text> 
+                    <Button
+                        onPress={() => //Built in prop, toggle a new state value called "showCalender" by using the logical not operator
+                            this.setState({showCalendar: !this.state.showCalendar})
+                        }
+                        title={this.state.date.toLocaleDateString('en-US')}//Built in prop, 
+                        color='#5637DD' //built in prop
+                        accessibilityLabel='Tap me to select a reservation date' //Helps with screen readers
+                    />
+                </View>
+                {this.state.showCalendar && ( //Only want to show the calendar if the "showCalendar" state property is set to true from the user clicking the <Button> above. Can use the logical and operator. When the logical and operator is used, if the left side operand is false, then the right side operand is not evaluated at all. Use { } to embed JS inside JSX to say "showCalendar && DateTimePicker". If "showCalendar" is false, the logical and will stop evaluating and not show <DateTimePicker> at all.
+                    <DateTimePicker
+                        value={this.state.date} //Built in prop, give it a value of the date stored in the state
+                        mode={'date'} //Built in prop
+                        display='default' //Built in prop
+                        onChange={(event, selectedDate) => { //Built in prop, this will cause the selectedDate to be saved to the state.
+                            selectedDate && this.setState({date: selectedDate, showCalendar: false}); //When a date is selected, the calendar will also be hidden. If the user cancels out the calendar instead of choosing a date, the set of the date can be undefined, so, use the logical and operator to make sure that the state is set to the selected date only if the "selectedDate" is truthy.
+                        }}
+                        style={styles.formItem}
+                    />
+                )}
+                <View style={styles.formRow}>
+                    <Button 
+                        onPress={() => this.handleReservation()} //Built in prop, use the event handler we created above
+                        title='Search' //Built in prop, gives button a title
+                        color='#5637DD' //Built in prop, give button a color
+                        accessibilityLabel='Tap me to search for available campsites to reserve' //for screen readers
+                    />
+                </View>
+            </ScrollView>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    formRow: {
+        alignItems: 'center', 
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        margin: 20
+    },
+    formLabel: {
+        fontSize: 18,
+        flex: 2 //applied to the labels, the label will take up twice as much of the row as the picker
+    },
+    formItem: {
+        flex: 1 //applied to pickers and switch, the picker will then take up half as much of the row as the label
+    }
+});
+
+export default Reservation;
