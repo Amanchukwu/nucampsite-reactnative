d2ea70fdee6448a09ae56de9655bebbd114603f2
SteveDev <stevehoneck@gmail.com>
2021-03-15 17:49:47 -0400

    Post 'Exercise: Animations'

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, Animated } from 'react-native';
 import { Card } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -43,13 +43,35 @@ function RenderItem(props) { //Need to pass in the "isLoading" and "errMess" pro
 
 class Home extends Component {
 
+    constructor(props) { //Constuctor must be added because animated value needs to be stored in the local state
+        super(props);
+        this.state = {
+            scaleValue: new Animated.Value(0) //Make a new animated value that is starting at 0, it is being named "scaleValue" but can be named anything.
+        };
+    }
+
+    animate() { //A method we are creating to store the animation so it can be called. The name "animate" is arbitrary and can be named anything.
+        Animated.timing( //Give this method 2 arguments, 1st is the name of the animated value that we want to have change over time, 2nd argument is an object that contains 3 properties
+            this.state.scaleValue,
+            {
+                toValue: 1, //Built in property, What we want the animated value to change to from its initial value. 1 = 100% in terms of scale
+                duration: 1500, //Built in property, how long to animate
+                useNativeDriver: true //Built in property, helps improve the performance of animations
+            }
+        ).start(); //Chain a built in method called "start()" to run the animation. (The timing method also has a "stop" method that can be chained to it)
+    }
+
+    componentDidMount() { //To start the animation and have it run just 1 time, will call the "animate" method we created within the React lifecycle method "componentDidMount". When the component mounts, it will automatically start the animation.
+        this.animate();
+    }
+
     static navigationOptions = { //Sets up screen title
         title: 'Home'
     }
 
     render() {
         return ( //NOTE FOR LINE 41: Can be used to render groups of items like FlatList. ScrollView loads all at once, FlatList just renders the visible part
-            <ScrollView> 
+            <Animated.ScrollView style={{transform: [{scale: this.state.scaleValue}]}} /*To connect the animation to the rendered component, we changed <ScrollView> to <Animated.ScrollView> which gives us a <ScrollView> component that is hooked up to the Animated API. Set the style using a "transform" style and specify the type of transform we want is "scale" and set the value of the scale to "this.stat.scaleValue" which is a dynamic value that will change when the animation is being run*/> 
                 <RenderItem 
                     item={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]} //Takes campsite data from state and searches for the ".featured" attribute that is set to true.
                     isLoading={this.props.campsites.isLoading} //Pass the "isLoading" prop to the <RenderItem> component so it can be used for conditional checks
@@ -65,7 +87,7 @@ class Home extends Component {
                     isLoading={this.props.partners.isLoading}
                     errMess={this.props.partners.errMess}
                 />
-            </ScrollView>
+            </Animated.ScrollView>
         );
     }
 }
