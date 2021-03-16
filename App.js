430cd482ed1fc7c39cef3c9e3721ccde6e2aa126
SteveDev <stevehoneck@gmail.com>
2021-03-16 18:17:23 -0400

    Post 'Exercise: Persist Redux Store'

--- App.js
+++ App.js
@@ -3,13 +3,19 @@ import React from 'react';
 import Main from './components/MainComponent';
 import { Provider } from 'react-redux'; //Imported to connect the redux store to react
 import { ConfigureStore } from './redux/configureStore';//Function we created which creates configures and returns the redux store
+import { PersistGate } from 'redux-persist/es/integration/react';
+import Loading from './components/LoadingComponent';
 
-const store = ConfigureStore(); //Create the redux store by calling "ConfigureStore" into this variable "store"
+const { persistor, store } = ConfigureStore(); //Bring in the "persistor" and the "store" objects from "ConfigureStore()" using object destructuring syntax. When called just "const store =" the note was: Create the redux store by calling "ConfigureStore" into this variable "store". 
 
 export default function App() {
   return ( //Wrap the <Main> component with the <Provider> component and pass the <Provider> component the store calling it "store". Gives the <Main> component and all its children the ability to connect to the redux store. 
     <Provider store={store}>
-      <Main />
+      <PersistGate //wrap the <Main> component in <PersistGate>, it prevents the application from rendering until the redux store has "rehydrated" fully from the client side storage
+        loading={<Loading />} //"loading" is a built in property, whatever component is passed into it is what will show while the redux store is rehydrating
+        persistor={persistor} /*must be passed the "persistor" object as well*/>
+        <Main />
+      </PersistGate>
     </Provider>
   );
 }
--- redux/configureStore.js
+++ redux/configureStore.js
@@ -6,19 +6,29 @@ import { comments } from './comments';
 import { promotions } from './promotions';
 import { partners } from './partners';
 import { favorites } from './favorites';
+import { persistStore, persistCombineReducers } from 'redux-persist'; //Added for redux persist. Add persistent support for reducers.
+import storage from 'redux-persist/es/storage'; //Added for redux persist. Gives access to local storage on device and adds storage support
+
+const config = { //Needed for "persist", first 2 properties are required
+    key: 'root', 
+    storage, //this property is for storage, we are putting in the "storage" that we imported above. There are other types of storage depending on use case.
+    debug: true //THis causes redux-persist to console log messages that will help with debugging.
+}
 
 //This function returns the result of calling the redux function "createStore" with a "combineReducers" argument that combines all the reducers into a single root reducer. Along with a call to applyMiddleware to enable redux-thunk and redux-loger to work
 export const ConfigureStore = () => {
     const store = createStore(
-        combineReducers({
+        persistCombineReducers(config, { //Changed from "combineReducers" and pass "config" object from above for first argument. Now "persistCombineReducers" function will handle updating the state to local storage whenever a reducer is used to update the redux store
             campsites,
             comments,
             partners,
-            promotions, 
+            promotions,
             favorites
         }),
         applyMiddleware(thunk, logger)
     );
 
-    return store;
-}
+    const persistor = persistStore(store); //"persistor" variable is set up by calling built in function "persistStore" passing "store" as an argument. THis enables the store to be persisted and we'll need to use this variable later in App.js.
+
+    return { persistor, store }; //return an object that contains both the "persistor" and the "store" so we can access both from App.js
+};
